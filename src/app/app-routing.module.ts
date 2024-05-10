eb47346bab6ea1d4ab617e028096fcb7bc224f64
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-10 13:47:54 +0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",  pathMatch:'full', component: HomeComponent},
+  {path:"create", component: CreateComponent},
+  {path:"edit/:userId", component: EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/types/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    _id: string,
+    firstName: string,
+    lastName: string,
+    profession: string,
+    gender: string,
+    image: string,
+    dateOfBirth: 26
+};
