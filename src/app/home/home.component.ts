5292079535778a82eb99f5cd5e291dee116320f0
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-10 14:37:58 +0300

    adding service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  users: User[] | null = []
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data
+    })
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  url = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+    return this.http.get<User[]>(this.url + '/users');
+  }
 }
