270193e7ea4ee9afff86eecca6fcf2debef7e15d
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-10 16:46:34 +0300

    adding form and little logic

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      day: [''],
+      month: [''],
+      year: ['']
+    });
+  }
+
+  onSubmit() {
+    const dob = {
+      day: this.form.value.day,
+      month: this.form.value.month,
+      year: this.form.value.year
+    };
+  }
+  
 }
