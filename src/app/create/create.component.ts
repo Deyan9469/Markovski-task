9891601c67391c521b571bcf8c4d77cb6ed05898
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-10 17:32:08 +0300

    adding form control for values

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
 })
+
 export class CreateComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) { 
+
+  }
 
   ngOnInit(): void {
     this.form = this.fb.group({
+      firstName:[''],
+      lastName:[''],
+      profession:[''],
+      gender:[''],
+      image: [''],
       day: [''],
       month: [''],
       year: ['']
@@ -20,11 +29,27 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit() {
-    const dob = {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let calcAge = currentYear - this.form.value.year;
+
+    let id = Math.floor(Math. random() * 100).toString();
+
+    const userData: User = {
+      firstName: this.form.value.firstName,
+      lastName: this.form.value.lastName,
+      profession: this.form.value.profession,
+      gender: this.form.value.gender,
       day: this.form.value.day,
       month: this.form.value.month,
-      year: this.form.value.year
+      year: this.form.value.year,
+      image: this.form.value.image,
+      _id: id,
+      age: calcAge,
     };
+    console.log(userData);
+    console.log(calcAge);
+    console.log(id);
   }
   
 }
