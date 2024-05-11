46036d49459204d1beae33c7a4185320fa34a42d
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-11 16:38:16 +0300

    adding valid validation

--- src/app/validators/input-validator.ts
+++ src/app/validators/input-validator.ts
@@ -4,7 +4,7 @@ export function inputValidator(): ValidatorFn {
     const regExp = new RegExp(`^[a-zA-Z]+$`);
 
     return (control) => {
-        const formControl = control.value === '' || regExp.test(control.value)
+        const formControl = regExp.test(control.value)
 
         return formControl ? null : { inputValidator: true };
     };
