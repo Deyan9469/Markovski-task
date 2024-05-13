8eaba220436c7978ddf0427cca88aa90643a0df9
Deyan9469 <deqnpavlikov@gmail.com>
2024-05-13 05:40:40 +0300

    adding pagination and styles
    styles for the Users cards and the pagination content(buttons)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../services/user.service';
 import { User } from '../types/user';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { PopupComponent } from '../popup/popup.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,15 +12,53 @@ import { PopupComponent } from '../popup/popup.component';
 })
 export class HomeComponent implements OnInit {
   users: User[] | null = [];
+  pagedUsers: User[] = [];
   modalRef!: NgbModalRef;
   userIdToDelete: string = '';
+  currentPage = 1;
+  pageSize = 3;
 
-  constructor(private userService: UserService, private modalService: NgbModal) { }
+  constructor(
+    private userService: UserService,
+    private modalService: NgbModal,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((data) => {
       this.users = data;
+      this.route.queryParams.subscribe(params => {
+        this.currentPage = params['page'] ? +params['page'] : 1;
+        this.updatePagedUsers();
+      });
     });
+  }
+
+  updatePagedUsers(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedUsers = this.users ? this.users.slice(startIndex, endIndex) : [];
+  }
+
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.router.navigate([], { queryParams: { page: page }, queryParamsHandling: 'merge' });
+      this.currentPage = page;
+      this.updatePagedUsers();
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil((this.users?.length || 0) / this.pageSize);
+  }
+
+  get pages(): number[] {
+    const pagesArray: number[] = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pagesArray.push(i);
+    }
+    return pagesArray;
   }
 
   onDelete(id: string): void {
